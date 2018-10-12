5ae468ec83c99dc66b8266f530339b473a96dea7
Jeremy <ayumi2183@gmail.com>
2018-10-12 16:05:29 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+
+    <!-- 組件使用 -->
+    <ListView></ListView>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+//導入組件
+import ListView from './components/List'
+
+export default {
+  name: 'App',
+  components: { //註冊組件
+      ListView
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="view">
+		<h1>
+			{{title}}
+		</h1>		
+	</div>
+</template>
+
+<script>
+//new Vue()
+export default{
+	name: "List",
+	// data: function(){  ES5
+	// 	return {}
+	// }
+	data(){  //ES6
+		return{
+			title: "TodoList",
+		}
+	}
+}
+</script>
+
+<style>
+	.view{
+		width: 400px;
+		border: 1px solid gray;
+		margin: 20px auto;
+	}
+</style>
